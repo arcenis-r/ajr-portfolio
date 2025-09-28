de766f953b6b697af678ea4cb9eb45676364d66b
arcenis-r <arcenis.rojas@gmail.com>
2025-09-28 00:43:56 -0500

    updated code in post about opening the ML blackbox

--- posts/20210829-open-blackbox-tidy/tidy-ml-evaluation-funs.R
+++ posts/20210829-open-blackbox-tidy/tidy-ml-evaluation-funs.R
@@ -1,6 +1,6 @@
 # Create a ggplot2 theme to use throughout the project
 ml_eval_theme <- function() {
-  theme_bw() + 
+  theme_bw() +
     theme(
       plot.title = element_text(hjust = 0.5, size = 18),
       strip.text = element_text(size = 12, color = "white"),
@@ -13,10 +13,10 @@ ml_eval_theme <- function() {
 #   and lambda was not equal to 0. In the case of home prices the first is
 #   true.
 rev_yj <- function(y_tf, lam) {
-  if(lam == 0) {
+  if (lam == 0) {
     exp(y_tf) - 1
   } else {
-    (((y_tf * lam) + 1) ^ (1 / lam)) - 1
+    (((y_tf * lam) + 1)^(1 / lam)) - 1
   }
 }
 
@@ -30,14 +30,14 @@ tidy_pred <- function(object, newdata) {
 get_var_imp <- function(wflow) {
   mod <- pull_workflow_fit(wflow)
   train_dat <- pull_workflow_mold(wflow) |> pluck("predictors")
-  target_dat <- pull_workflow_mold(wflow) |> 
-    pluck("outcomes") |> 
+  target_dat <- pull_workflow_mold(wflow) |>
+    pluck("outcomes") |>
     pull(sale_price)
-  
+
   vi_df <- vip::vi_permute(
-    mod, 
-    train = train_dat, 
-    target = target_dat, 
+    mod,
+    train = train_dat,
+    target = target_dat,
     metric = "mae",
     pred_wrapper = tidy_pred,
     nsim = 10,
@@ -50,7 +50,7 @@ get_shap <- function(wflow) {
   fastshap::explain(
     extract_fit_parsnip(wflow),
     X = extract_mold(wflow) |> pluck("predictors") |> as.matrix(),
-    feature_names = extract_mold(wflow) |> 
+    feature_names = extract_mold(wflow) |>
       pluck("predictors") |>
       colnames(),
     pred_wrapper = tidy_pred,
@@ -64,8 +64,8 @@ get_shap_imp <- function(shap_obj) {
     as_tibble() |>
     summarise(across(everything(), \(x) mean(abs(x)))) |>
     pivot_longer(
-      everything(), 
-      names_to = "Variable", 
+      everything(),
+      names_to = "Variable",
       values_to = "Importance"
     ) |>
     arrange(desc(Importance))
@@ -77,16 +77,17 @@ get_shap_summary <- function(vi, shap_df, feat_df, max_features = 20) {
   vi <- vi %>%
     set_names(colnames(.) |> str_to_lower()) |>
     slice_max(importance, n = max_features, with_ties = FALSE)
-  
+
   shap_df <- shap_df |>
     as_tibble() |>
     mutate(id = row_number()) |>
-    pivot_longer(cols = -id, names_to = "variable", values_to = "shap_value")
-  
-  feat_df <- feat_df |> 
+    pivot_longer(cols = -id, names_to = "variable", values_to = "shap_value") |>
+    mutate(shap_value = as.numeric(shap_value))
+
+  feat_df <- feat_df |>
     mutate(id = row_number()) |>
     pivot_longer(cols = -id, names_to = "variable", values_to = "feature_value")
-  
+
   left_join(shap_df, feat_df, by = c("variable", "id")) |>
     right_join(vi, by = "variable") |>
     mutate(
@@ -99,11 +100,11 @@ get_shap_summary <- function(vi, shap_df, feat_df, max_features = 20) {
 
 # Generate a variable importance plot
 plot_var_imp <- function(vi_df, algorithm, max_features = 20) {
-  vi_plot_df <- vi_df %>% 
+  vi_plot_df <- vi_df %>%
     slice_max(Importance, n = max_features) %>%
     arrange(desc(Importance))
-  
-  vi_plot_df %>% 
+
+  vi_plot_df %>%
     vip::vip() +
     labs(title = str_glue("Variable Importance: {algorithm}")) +
     ml_eval_theme()
@@ -116,7 +117,7 @@ plot_shap_summary <- function(shap_summary_df, algorithm) {
       aes(shap_value, fct_rev(fct_inorder(variable)), color = feature_value)
     ) +
     geom_jitter(height = 0.2, alpha = 0.8) +
-    geom_vline(xintercept = 0, size = 1) +
+    geom_vline(xintercept = 0, linewidth = 1) +
     scale_color_gradient(low = "blue", high = "red") +
     labs(
       title = str_glue("SHAP Summary Plot: {algorithm}"),
@@ -132,14 +133,14 @@ plot_pdp <- function(wflow, pred_var, algorithm) {
   pred_var <- enquo(pred_var)
   mod <- extract_fit_parsnip(wflow)
   train_dat <- extract_mold(wflow) |> pluck("predictors")
-  
+
   pdp::partial(
     mod,
     pred.var = quo_name(pred_var),
     train = train_dat,
     type = "regression"
   ) |>
-    data.frame() |> 
+    data.frame() |>
     ggplot(aes(!!pred_var, yhat)) +
     geom_line() +
     stat_smooth(method = "loess", formula = y ~ x) +
@@ -155,11 +156,11 @@ plot_pdp <- function(wflow, pred_var, algorithm) {
 # and accompanying contribution plot
 get_contributions <- function(shap_df, algorithm, rnum, nfeat) {
   contrib_df <- shap_df |>
-    as_tibble() |> 
+    as_tibble() |>
     mutate(across(everything(), as.numeric)) |>
     dplyr::slice(rnum) |>
     pivot_longer(everything(), names_to = "variable", values_to = "shap")
-  
+
   contrib_plot <- contrib_df |>
     slice_max(abs(shap), n = nfeat, with_ties = FALSE) |>
     ggplot(aes(x = shap, y = fct_reorder(variable, abs(shap)), fill = shap)) +
@@ -172,6 +173,6 @@ get_contributions <- function(shap_df, algorithm, rnum, nfeat) {
       )
     ) +
     ml_eval_theme()
-  
+
   return(list(contrib_df = contrib_df, contrib_plot = contrib_plot))
 }
